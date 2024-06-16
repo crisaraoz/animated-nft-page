bcd5f4c6bed1b32ffb4d7616366a4c19fe3095c4
Cristian Iván Aráoz <araozcristian@gmail.com>
2024-06-16 19:12:17 -0300

    se agrega componente footer

---
+++ app/Components/Footer/Footer.tsx
@@ -0,0 +1,200 @@
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+import logo from "../../../public/logo.svg";
+import footer from "../../utils/footer";
+import Image from "next/image";
+
+import {
+  AiFillGithub,
+  AiFillTwitterCircle,
+  AiFillInstagram,
+  AiFillLinkedin,
+} from "react-icons/ai";
+import { BsDiscord } from "react-icons/bs";
+
+function Footer() {
+  return (
+    <FooterStyled>
+      <nav>
+        <div className="logo-con">
+          <div className="logo">
+            <Image src={logo} width={36} alt="logo" />
+            <h2>Logo</h2>
+          </div>
+          <div className="input">
+            <input type="text" placeholder="Subscribe here..." />
+            <button>&#8594;</button>
+          </div>
+          <p>
+            Create, Sell And Collect Truly Rare Digital Artworks. Powered By
+            Blockchain Technology.
+          </p>
+          <div className="socials">
+            <AiFillGithub />
+            <AiFillTwitterCircle />
+            <AiFillInstagram />
+            <BsDiscord />
+            <AiFillLinkedin />
+          </div>
+        </div>
+        <div className="links">
+          {footer.map((item: any, index: number) => {
+            return (
+              <div key={index}>
+                <h4>{item.title}</h4>
+                <ul>
+                  {item.links.map((text: any, index: number) => {
+                    return (
+                      <li key={index}>
+                        <a href="">{text.name}</a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+      </nav>
+      <p className="rights">
+        <a href="">©{new Date().getFullYear()} Cristian Araoz. All rights reserved.</a>
+        <a href="">Privacy policy | Terms of service</a>
+      </p>
+    </FooterStyled>
+  );
+}
+
+const FooterStyled = styled.footer`
+  position: relative;
+  z-index: 10;
+  padding: 3rem 5rem 0 5rem;
+  background-color: var(--color-bg);
+  border-top: 1px solid var(--color-border);
+
+  nav {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+
+    .logo-con {
+      display: flex;
+      flex-direction: column;
+      gap: 1.5rem;
+      width: 100%;
+
+      .input {
+        position: relative;
+      }
+
+      .input input {
+        width: 100%;
+        padding: 1rem 0.8rem;
+        border-radius: 8px;
+        background-color: #161616;
+        border: 1px solid var(--color-border);
+        &::placeholder {
+          color: var(--color-border);
+          font-weight: 500;
+        }
+      }
+
+      .input button {
+        position: absolute;
+        right: 1rem; 
+        top: 50%;
+        transform: translateY(-50%);
+        background: transparent;
+        cursor: pointer;
+        transition: all 0.3s ease-in-out;
+        &:hover {
+          color: white;
+          transform: translateY(-50%) translateX(0.5rem);
+        }
+      }
+
+      .socials {
+        display: flex;
+        gap: 1rem;
+        font-size: 1.6rem;
+        svg {
+          cursor: pointer;
+          transition: all 0.3s ease-in-out;
+          &:hover {
+            color: white;
+          }
+        }
+      }
+
+      .logo {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+      }
+    }
+
+    .links {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+      gap: 2rem;
+      
+      h4 {
+        font-size: 1.5rem;
+        margin-bottom: 1rem;
+      }
+
+      li:not(:last-child) {
+        margin-bottom: 0.5rem;
+      }
+
+      a {
+        transition: all 0.3s ease-in-out;
+        &:hover {
+          color: white;
+        }
+      }
+    }
+  }
+
+  .rights {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    margin-top: 2rem;
+    padding: 1rem 0;
+    border-top: 1px solid var(--color-border);
+
+    a {
+      margin: 0.5rem 0;
+    }
+  }
+
+  @media (min-width: 768px) {
+    padding: 5rem 10rem 0 10rem;
+
+    nav {
+      flex-direction: row;
+      justify-content: space-between;
+      gap: 3rem;
+
+      .logo-con {
+        width: 50%;
+      }
+
+      .links {
+        flex: 1;
+        grid-template-columns: repeat(3, 1fr);
+      }
+    }
+
+    .rights {
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+    }
+  }
+`;
+
+export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import styles from "./page.module.css";
 import Hero from "./Components/Hero/Hero";
 import styled from "styled-components";
 import Fullpage from "./Components/FullPage/FullPage";
+import Footer from "./Components/Footer/Footer";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
     <MainStyled>
       <Fullpage />
     </MainStyled>
+    <Footer />
     </>
   );
 }
